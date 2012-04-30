e27c8dc2815e72c2e2474e7990e0b65d1f67dfa8
Guillermo Gonzalez <guillermog@tricuspide.com>
2012-04-29 21:00:55 -0400

    Adaptacion siteminder OK

--- siteminder/Access_Siteminder.php
+++ siteminder/Access_Siteminder.php
@@ -15,15 +15,19 @@ class Access_Siteminder {
 			/*
 				This is the configuration used for testing
 			*/
-			$this->username = "agent";
-			$this->password = "11madrid11";		
-			$this->ext_siteminder_iHotelier = 41;
+			$this->username = "agent2";
+			$this->password = "5ramblas5";		
+			$this->ext_siteminder_iHotelier = 122;
 		}
 		
-		$this->referer = "https://www.siteminder.co.uk/web/login";
+		$this->default_referer = "https://www.siteminder.co.uk/web/login";
 		$this->cookie_file = $_SERVER["DOCUMENT_ROOT"]."/cache/siteminder_".md5(uniqid()).".cookie";
 	}
-	private function init_curl($url, $method = 1) {
+	private function init_curl($url, $method = 1, $referer = null) {
+		if (!$referer) {
+			$referer	 = $this->default_referer;
+		}
+		
 		$curl_dscr = curl_init($url);
 		curl_setopt($curl_dscr, CURLOPT_SSL_VERIFYPEER, FALSE);
 		curl_setopt($curl_dscr, CURLOPT_HEADER, 0);
@@ -33,7 +37,7 @@ class Access_Siteminder {
 		curl_setopt($curl_dscr, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($curl_dscr, CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.19) Gecko/2010031422 Firefox/3.0.19");
 		curl_setopt($curl_dscr, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl_dscr, CURLOPT_REFERER, $this->referer);
+		curl_setopt($curl_dscr, CURLOPT_REFERER, $referer);
 
 		return $curl_dscr;
 	}
@@ -78,8 +82,16 @@ class Access_Siteminder {
 		$cells["_method"] = "PUT";
 		$cells["state[y]"] = "114";
 		$cells["org.codehaus.groovy.grails.SYNCHRONIZER_URI"] = "/web/extranet/inventory/".$this->ext_siteminder_iHotelier;
-		$cells["org.codehaus.groovy.grails.SYNCHRONIZER_TOKEN"] = "9709e456-6014-4f04-93a1-2d56da28e9b0";
+		$cells["org.codehaus.groovy.grails.SYNCHRONIZER_TOKEN"] = $this->getToken();
 		return $this->send_post($cells);
+	}
+	function getToken() {
+		$url_fetch = "https://www.siteminder.co.uk/web/extranet/inventory/".$this->ext_siteminder_iHotelier;
+		$ch = $this->init_curl($url_fetch, 0);
+		$info = curl_exec($ch);
+		$html = phpQuery::newDocument($info);
+		$token_value = $html->find('input[name="org.codehaus.groovy.grails.SYNCHRONIZER_TOKEN"]')->val();
+		return $token_value;
 	}
 	function send_post($mixed) {
 		
@@ -90,7 +102,7 @@ class Access_Siteminder {
 
 		foreach ( $mixed as $id => $val ) $post .= "&$id=$val";
 
-		$ch = $this->init_curl($url_post, 1);
+		$ch = $this->init_curl($url_post, 1, $url_post);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, substr($post, 1));
 		$info = curl_exec($ch);
 		$this->uninit_curl($ch);
--- siteminder/test.php
+++ siteminder/test.php
@@ -1,15 +1,14 @@
 <?
 	include("fetchid_Siteminder.php");
 	include("Access_Siteminder.php");
-	include("../phpQuery/phpQuery.php");
+	include($_SERVER["DOCUMENT_ROOT"]."/sites/default/lib/phpQuery/phpQuery.php");
 
 
 	$a_siteminder = new Access_Siteminder();
 	
 	if ( $a_siteminder->login() ) {
 		echo "logged";
-		echo $a_siteminder->fetch('2010-08-06');
-		echo var_dump($a_siteminder->send_stock(array("hrtda_149362" => 2, "hrtda_149763" => 3)));
+		echo var_dump($a_siteminder->send_stock(array("hrtds[2238714]" => 0)));
 	}
 	else {
 		echo "login not successfull";	
