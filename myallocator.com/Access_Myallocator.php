c5d095ab4404cb82fd52179b1ee6b6a6fef12310
Guillermo Gonzalez <guillermog@tricuspide.com>
2014-06-17 02:55:50 +0200

    Test MyAllocator Deactivate Venere, LastMinute, NuSOAP

--- myallocator.com/Access_Myallocator.php
+++ myallocator.com/Access_Myallocator.php
@@ -2,22 +2,51 @@
 
 class Access_Myallocator {
 
+	public $UserId = "agent";
+	public $UserPassword = "11madrid11";
 
     function __construct($stockupdate) {
     	$this->stockupdate = $stockupdate;
     }
+    function test() {
+		  $xml = "<?xml version=\"1.0\"?>
+		    <GetProperties>
+		      <Auth>
+		        <UserId>".$this->UserId."</UserId>
+		        <UserPassword>".$this->UserPassword."</UserPassword>
+		        <VendorId>madridac</VendorId>
+		        <VendorPassword>7ENk42zQLN</VendorPassword>
+		      </Auth> 
+		    </GetProperties>";
 
+			//exit($xml[$i]);
+			$postfields = array(
+			  'xmlRequestString' => $xml
+			);
+			// We send XML via CURL using POST with a http header of text/xml.
+			$ch = curl_init();
+			// set URL and other appropriate options
+			curl_setopt($ch, CURLOPT_URL, "http://api.myallocator.com");
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$ch_result = curl_exec($ch);
+			curl_close($ch);
+			// Print CURL result.
+
+			$xml = simplexml_load_string($ch_result);
+			return count($xml->Properties) > 0;
+    }
     function SetAllocation($from, $to, $stock, $rate = null) {
-		$UserId = "agent";
-		$UserPassword = "11madrid11";
 		if (!is_object($this->stockupdate->syncconnection)) {
 			$this->stockupdate->log_Event("No syncconnection for".$this->stockupdate->syncconnection);
 		}
 		$xml_request = "<?xml version=\"1.0\"?> 
 		<SetAllocation>
 		<Auth>
-		  <UserId>".$UserId."</UserId> 
-		  <UserPassword>".$UserPassword."</UserPassword> 
+		  <UserId>".$this->UserId."</UserId> 
+		  <UserPassword>".$this->UserPassword."</UserPassword> 
 		  <PropertyId>".$this->stockupdate->syncconnection->myallocator_propertyid."</PropertyId> 
 		  <VendorId>madridac</VendorId> 
 		  <VendorPassword>7ENk42zQLN</VendorPassword>
